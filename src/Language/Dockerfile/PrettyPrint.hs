f5202372dc42b43f9145a6e0a3c198556dcb113b
yamadapc <tacla.yamada@gmail.com>
2016-08-06 20:50:28 -0300

    Workspace update

--- src/Language/Dockerfile/PrettyPrint.hs
+++ src/Language/Dockerfile/PrettyPrint.hs
@@ -11,14 +11,7 @@ import           Prelude                    hiding (return, (>>), (>>=))
 import qualified Prelude                    ((>>), (>>=))
 import           Text.PrettyPrint
 
-test :: IO ()
-test = parseFile "./Dockerfile" Prelude.>>=
-    (\(Right input) ->
-       print input Prelude.>>
-       putStrLn (prettyPrint input) Prelude.>>
-       putStrLn "-----" Prelude.>>
-       (putStrLn Prelude.=<< readFile "./Dockerfile"))
-
+--| Pretty print a 'Dockerfile' to a 'String'
 prettyPrint :: Dockerfile -> String
 prettyPrint = unlines
     . reverse . snd . foldl removeDoubleBlank (False, [])
@@ -30,6 +23,7 @@ prettyPrint = unlines
     removeDoubleBlank (False, m) "" = (True, "":m)
     removeDoubleBlank (_, m) s = (False, s:m)
 
+--| Pretty print a 'InstructionPos' to a 'String'
 prettyPrintInstructionPos :: InstructionPos -> String
 prettyPrintInstructionPos (InstructionPos i _ _) = render (prettyPrintInstruction i)
 
