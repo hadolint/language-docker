ad9e0396796c02d940167b63bd572224035249b4
yamadapc <tacla.yamada@gmail.com>
2016-10-24 10:58:09 -0200

    Fix compilation with ShellCheck-0.4.5
    This is related to fpco/stackage#2007

--- src/Language/Dockerfile/Bash.hs
+++ src/Language/Dockerfile/Bash.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Language.Dockerfile.Bash where
 
 import           Data.Functor.Identity (runIdentity)
@@ -6,9 +7,19 @@ import           ShellCheck.Interface
 
 shellcheck :: String -> [Comment]
 shellcheck bashScript = map comment $ crComments $ runIdentity $ checkScript si spec
-    where comment (PositionedComment _ c) = c
-          si = mockedSystemInterface [("","")]
-          spec = CheckSpec filename script exclusions (Just Bash)
-          script = "#!/bin/bash\n" ++ bashScript
-          filename = "" -- filename can be ommited because we only want the parse results back
-          exclusions = []
+  where
+    si = mockedSystemInterface [("","")]
+    spec = CheckSpec filename script exclusions (Just Bash)
+    script = "#!/bin/bash\n" ++ bashScript
+    filename = "" -- filename can be ommited because we only want the parse results back
+    exclusions = []
+#ifdef MIN_VERSION_ShellCheck
+#  if MIN_VERSION_ShellCheck(0,4,5)
+    comment (PositionedComment _ _ c) = c
+#  else
+    comment (PositionedComment _ c) = c
+#  endif
+#else
+    comment (PositionedComment _ _ c) = c
+{-# WARNING shellcheck "Cabal macro to detect ShellCheck - version not defined, assuming ShellCheck>0.4.5" #-}
+#endif
