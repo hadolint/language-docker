dd1509eeabe22ca68a17525c1dbf8d9984e04482
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2018-03-25 00:36:15 +0100

    Even fewer dependencies
    I figured a way to have even less dependencies for this package

--- src/Language/Docker/Syntax/Lift.hs
+++ src/Language/Docker/Syntax/Lift.hs
@@ -5,11 +5,14 @@ module Language.Docker.Syntax.Lift where
 import Data.Fixed (Fixed)
 import Data.List.NonEmpty (NonEmpty)
 import Data.Time.Clock (DiffTime)
-import Instances.TH.Lift ()
 import Language.Haskell.TH.Lift
 import Language.Haskell.TH.Syntax ()
+import qualified Data.ByteString as ByteString
 
 import Language.Docker.Syntax
+
+instance Lift ByteString.ByteString where
+  lift b = [| ByteString.pack $(lift $ ByteString.unpack b) |]
 
 deriveLift ''NonEmpty
 
